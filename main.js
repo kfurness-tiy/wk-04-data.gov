b6ba780c8789d6299cf6b5126785b47b3b3dbbe8
kelly <kelly.n.waite@gmail.com>
2016-10-18 12:45:15 -0600

    feat(core) Add bootstrap; Clean code; Add new promise
    Add bootstrap with CDN since I could not get files to work
    Switched to jQuery promise

--- main.js
+++ main.js
@@ -1,78 +1,11 @@
-// function salaryData(data) {
-//   ourData = data;
-//   console.log(ourData[0].salary);
-//   console.log(data);
-// }
+'use strict';
 
-function dataLoad () {
-  return new Promise( function(resolve, reject) {
-    setTimeout( function() {
-      if (ourData) {
-        resolve('got it');
-      }
-      else {
-        reject('no go');
-      }
+ //http://www.w3schools.com/jsref/met_table_insertrow.asp
 
-    }, 2000);
-  })
-};
-
-var loaded = dataLoad();
-
-loaded.then(function(result) {
-  console.log(ourData);
-  printData(ourData);
-  }, function(err) {
-    console.log(err + ': Try again');
-});
-
-function printData (ourData) {
-  var data = ourData;
-  console.log(data[0].salary);
-  createTable(data);
-  // randomData(data);
-  }
-
-
-//http://www.w3schools.com/jsref/met_table_insertrow.asp
-
-
+// TODO Change to map
 function createTable (data) {
   for (var d = 0; d < data.length; d++) {
-      var msg = '<tr><td>' + data[d].position_title + '</td>' + '<td>' + data[d].salary + '</td></tr>\n';
+      var msg = '<tr><td class="position">' + data[d].position_title + '</td>' + '<td>' + data[d].salary + '</td></tr>\n';
       document.querySelector('.data').innerHTML += msg;
   }
 }
-
-// function createTable (data) {
-//   var body = document.getElementsByTagName('body')[0];
-//
-//   var tbl = document.createElement('table');
-//   var tblBody = document.createElement('tbody');
-//   tbl.style.width = '800px';
-//   tbl.style.border = '1px solid black';
-//
-//   for (let i = 0; i < 2; i++) {
-//     var row = document.createElement('tr');
-//
-//   for (var j = 0; j < 10; j++) {
-//     var cell = document.createElement('td');
-//     var cellText = document.createTextNode(data[j].salary);
-//     cell.appendChild(cellText);
-//     row.appendChild(cell);
-//   }
-//
-//     tblBody.appendChild(row);
-//   }
-//
-//   tbl.appendChild(tblBody);
-//   body.appendChild(tbl);
-//   tbl.setAttribute('boder', '2');
-// }
-//
-// function getData(data) {
-//   for (var d = 0; d < 10; d++ ) {
-//     return data[d].salary;
-//   }
-// }
--- salary-api.js
+++ salary-api.js
@@ -1,24 +1,26 @@
+'use strict';
 var ourData = {};
 
 window.onload = function salaries () {
 
   let url = 'https://open.whitehouse.gov/resource/wb62-w5sr.json'
 
-  // over 27,000 hits with the following
-  // $.get(url, function(data) {
-  //   console.log(data);
-  // });
-
-  // we'll do the long version to prevent overload
-  $.ajax({
+  var jQueryPromise = $.ajax({
     dataType: "json",
     url: url,
     data: {
       "$limit" : 25
     }
-  }).done(function(data) {
-     ourData = data;
-     return data;
   })
 
-};
+  var realPromise = Promise.resolve(jQueryPromise);
+
+  realPromise.then(function(ourData) {
+    var data = ourData;
+    console.log(data);
+    createTable(data);
+  }, function(err) {
+    console.log(err + ': Try again');
+  });
+
+}; //ends salaries()
